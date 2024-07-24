f028fa063a298bf8b2f790971d79d744bc6bc23b
Hanqing Wang <hqwang066@gmail.com>
2024-07-24 19:08:03 +0000

    debugged host_info and usage scripts

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -8,7 +8,7 @@ psql_user=$4
 psql_password=$5
 
 #check for # of args
-if ["$#" -ne 5]; then
+if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
 fi
@@ -17,25 +17,26 @@ vmstat_mb=$(vmstat --unit M)
 # save hostname as a variable
 hostname=$(hostname -f)
 #save cpu info as a variable
-lscpu_out=`lscpu`
+lscpu_out=$(lscpu)
 
 # Retrieve hardware specification variables
 # xargs is a trick to trim leading and trailing white spaces
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
-cpu_architecture=$(lscpu | grep Architecture | awk '{print $2}'| xargs)
-cpu_model= $(lscpu | grep 'Model name' | awk -F': ' '{print $2}'|xargs)
-cpu_mhz=$(grep -m 1 "cpu MHz" /proc/cpuinfo| awk -F': ' '{print $2}'|xargs)
-l2_cache=$(lscpu | grep 'L2 cache' | awk -F': ' '{print $2}'|xargs)
-total_mem=$(grep MemTotal /proc/meminfo| tail -1 |awk '{print $2}')
+cpu_architecture=$(echo "$lscpu_out"  | grep Architecture | awk '{print $2}'| xargs)
+cpu_model=$(echo "$lscpu_out"   | grep 'Model name' | awk -F': ' '{print $2}'| xargs)
+cpu_mhz=$(grep -m 1 "cpu MHz" /proc/cpuinfo| awk -F': ' '{print $2}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | grep 'L2 cache' | awk '{print $3}' | grep -o '^[0-9]\+')
+total_mem=$(grep MemTotal /proc/meminfo| tail -1 | awk '{print $2}')
 
 # Current time in `2019-11-26 14:40:19` UTC format
 timestamp=$(vmstat -t | awk 'NR==3 {print $18 " " $19}')
 
 # Subquery to find matching id in host_info table
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
 # PSQL command: Inserts server usage data into host_usage table
 # Note: be careful with double and single quotes
-insert_stmt="INSERT INTO host_info(timestamp, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem) VALUES('$timestamp', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$L2_cache', '$total_mem')
+insert_stmt="INSERT INTO host_info(hostname, timestamp, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem) VALUES('$hostname', '$timestamp', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem')"
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -7,8 +7,8 @@ db_name=$3
 psql_user=$4
 psql_password=$5
 
-#check for # of args
-if ["$#" -ne 5]; then
+# Check for the number of args
+if [ "$#" -ne 5 ]; then
   echo "Illegal number of parameters"
   exit 1
 fi
@@ -20,11 +20,11 @@ hostname=$(hostname -f)
 
 # Retrieve hardware specification variables
 # xargs is a trick to trim leading and trailing white spaces
-memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-cpu_idle=$(echo "$vmstat_mb" | tail -n1 | awk '{print $15}'| xargs)
-cpu_kernel=$(echo "$vmstat_mb" | tail -n1 | awk '{print $14}'| xargs)
-disk_io=$(echo "$vmstat_mb" | tail -n1 | awk '{print $10}'| xargs)
-disk_available=$(df -BM --output=avail /)
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}' | tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | tail -n1 | awk '{print $15}' | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | tail -n1 | awk '{print $14}' | xargs)
+disk_io=$(echo "$vmstat_mb" | tail -n1 | awk '{print $10}' | xargs)
+disk_available=$(df -BM --output=avail / | tail -n1 | xargs | sed s/[^0-9]*//g )
 
 # Current time in `2019-11-26 14:40:19` UTC format
 timestamp=$(vmstat -t | awk 'NR==3 {print $18 " " $19}')
@@ -33,12 +33,10 @@ timestamp=$(vmstat -t | awk 'NR==3 {print $18 " " $19}')
 host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 # PSQL command: Inserts server usage data into host_usage table
 # Note: be careful with double and single quotes
-insert_stmt="INSERT INTO host_usage(timestamp,memory_free,cpu_idle,cpu_kernel,disk_io,disk_available) VALUES('$timestamp','$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_available')
+insert_stmt="INSERT INTO host_usage(timestamp,memory_free,cpu_idle,cpu_kernel,disk_io,disk_available) VALUES('$timestamp','$memory_free','$cpu_idle','$cpu_kernel','$disk_io','$disk_available')";
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
 #Insert date into a database
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
-
-
